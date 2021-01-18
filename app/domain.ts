cd205f0dca9d0e5ec4cfa4ce6ecc0ac84dbda8c6
leojan <>
2021-01-18 12:33:28 +0800

    feat(app): enrich application
    - more information for debug

--- app/domain.ts
+++ app/domain.ts
@@ -2,11 +2,13 @@ import '@leo/lib/node'
 export async function instance() {
     const { instance } = await import('./instance');
     const app = await instance();
+    app.use(app.initialize());
     await app.load(await import('@leo/lib/config'));
     await app.load(await import('@leo/lib/config/doc'));
     await app.load(await import('@leo/lib/database'));
     await app.load(await import('@leo/lib/database/sequelize'));
     await app.load(await import('@leo/lib/connection'));
     await app.load(await import('@leo/lib/connection/http'));
+    app.use(app.finalize());
     return app;
 }
--- app/instance.ts
+++ app/instance.ts
@@ -1,3 +1,17 @@
+import { RequestHandler, ErrorRequestHandler } from 'express'
+{
+    const layer = require('express/lib/router/layer');
+    layer.prototype.handle_request = <RequestHandler>function (this: any, req, res, next) {
+        const fn = this.handle as RequestHandler;
+
+        if (fn.length > 3) {
+            return next();
+        }
+        Promise.resolve().then(function () {
+            return fn(req, res, next);
+        }).catch(next);
+    };
+}
 declare module 'express-serve-static-core' {
     /*
     !FIX
@@ -22,6 +36,14 @@ declare global {
         interface Factory<V> {
             (ctx: Express.Application): V
         }
+        interface Request {
+            ctx(): Express.Application
+            cid(): string
+            log(): Log
+            //ranges(maxsize: number): Range[]
+            //attempt<R>(fn: Message<R>): R
+            //querystring<R extends string | number | symbol>(name: string): R[] | undefined
+        }
         interface Application {
             readonly express: typeof import('express')
             emit(event: 'close'): boolean
@@ -33,9 +55,11 @@ declare global {
             object<R>(names: string[]): Optional<R>
             object<R>(names: string[], factory: Factory<R>): this
             cid(): string
-            log(name: string): Log
+            log(...names: string[]): Log
             load<R>(module: Module<R>): Promise<R>
-            load<R>(module: { default?: Module<R> }): Promise<R>
+            load<R>(module: { default: Module<R> }): Promise<R>
+            finalize(): ErrorRequestHandler[]
+            initialize(): RequestHandler[]
             synchronize<R>(name: string, fn: () => R | PromiseLike<R>): Promise<R>
         }
     }
@@ -56,55 +80,122 @@ export async function instance() {
         console.assert(factory);
         return factory;
     };
-    const map = {} as {
-        [name: string]: unknown
+    const got = function <R>(name: string) {
+        return app.get(name) as Optional<R>;
+    };
+    const err = function (e: Error) {
+        process.emit('warning', e);
     };
     const ctx = <Express.Application>{
-        //sequelize,
         express,
-        //axios,
+
         async synchronize(name, fn) {
             const key = `job:${name}`;
-            const job = map[key] as Promise<unknown> ?? Promise.resolve();
-            map[key] = job.catch((e) => { process.emit('warning', e); }).then(fn);
-            return map[key];
+            const job = got<Promise<unknown>>(key) ?? Promise.resolve();
+            return app.set(key, job.catch(err).then(fn)).get(key);
         },
-        async load<R>(module: Module<R> & { default?: Module<R> }) {
+        async load<R>(module: Module<R> & { default: Module<R> }) {
             if (typeof module.default === typeof Function) {
-                return this.load(module.default!);
+                return this.load(module.default);
             }
             return module(app);
         },
         cid() {
             return now.getTime().toString(36);
         },
-        log(name) {
+        log(...names) {
             return {
                 error(...a) {
-                    console.error(new Date().toISOString(), name, ...a);
+                    console.error(new Date().toISOString(), names, ...a);
                 },
                 info(e: unknown, ...a: unknown[]) {
-                    console.info(new Date().toISOString(), name, e, ...a);
+                    console.info(new Date().toISOString(), names, e, ...a);
                 },
             };
         },
         object<R>(names: string[], factory?: Express.Factory<R>): Optional<R> | Express.Application {
             return factory ? set(names, factory) : get<R>(names)?.(this);
         },
+        finalize() {
+            return [
+                function (err: Error, req, res, _) {
+                    req.log().error(err);
+
+                    if (!err.status) {
+                        err.status = 500;
+                    }
+                    if (err.retrydelay) {
+                        res.set('Retry-After', err.retrydelay.toString());
+                    }
+                    if (err.code) {
+                        res.statusMessage = err.code.toString();
+                    }
+                    res.status(err.status).json({
+                        errors: [
+                            err,
+                        ],
+                    });
+                },
+            ];
+        },
+        initialize() {
+            return [
+                function (req, res, next) {
+                    const now = new Date();
+
+                    req.cid = function () {
+                        return req.header('x-request-id')
+                            || req.header('x-correlation-id') as string;
+                    };
+                    if (!req.cid()) {
+                        const port = req.socket?.remotePort ?? 0;
+                        const time = now.getTime() + port + Math.random();
+                        const text = time.toString(36);
+                        req.cid = () => text;
+                    }
+                    const log = function (...names: string[]) {
+                        return ctx.log(...names, req.cid());
+                    };
+                    req.log = function () {
+                        const obj = log('http');
+                        req.log = () => obj;
+                        return obj;
+                    };
+                    req.ctx = function () {
+                        const cid = req.cid;
+                        const ctx = {
+                            ...req.app,
+                            log,
+                            cid,
+                        };
+                        req.ctx = function () {
+                            return ctx;
+                        };
+                        return ctx;
+                    };
+                    res.once('finish', function () {
+                        const elapse = new Date().getTime() - now.getTime();
+                        req.log().info({
+                            elapse,
+                            method: req.method,
+                            url: req.path,
+                            query: req.query,
+                            status: res.statusCode,
+                            headers: { ...res.getHeaders() },
+                        });
+                    });
+                    req.log().info({
+                        address: req.socket?.remoteAddress,
+                        port: req.socket?.remotePort,
+                        method: req.method,
+                        url: req.path,
+                        query: req.query,
+                        headers: req.headers,
+                    });
+                    return next();
+                },
+            ];
+        },
     };
     return Object.assign(app, ctx);
-}
-import { RequestHandler } from 'express'
-{
-    const layer = require('express/lib/router/layer');
-    layer.prototype.handle_request = <RequestHandler>function (this: any, req, res, next) {
-        const fn = this.handle as RequestHandler;
-
-        if (fn.length > 3) {
-            return next();
-        }
-        Promise.resolve().then(function () {
-            return fn(req, res, next);
-        }).catch(next);
-    };
 }
